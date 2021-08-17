303a6ed48e7ad0747619b3ce5efee9b833f9375b
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-08-17 12:22:16 +1000

    Refactored modulated functions, separating them into input and conversion functions to then feed into a display function which prints the degrees along with the specific measurement unit.

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -4,17 +4,30 @@ Pseudocode for temperature conversion
 """
 
 
-def celsius_to_farenheit():
-    celsius = float(input("Celsius: "))
-    fahrenheit = celsius * 9.0 / 5 + 32
-    print("Result: {:.2f} F".format(fahrenheit))
+def display(degrees, unit):
+    print("Result: {:.2f} {}".format(degrees, unit))
 
 
-def farenheit_to_censius():
-    # Hint: celsius = 5 / 9 * (fahrenheit - 32)
-    fahrenheit = float(input("Fahrenheit: "))
+def fahrenheit_to_celsius_conversion(fahrenheit):
     celsius = 5 / 9 * (fahrenheit - 32)
-    print("Result: {:.2f} C".format(celsius))
+    return celsius
+
+
+def celsius_to_fahrenheit_conversion(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+def celsius_to_fahrenheit_input():
+    celsius = float(input("Celsius: "))
+    fahrenheit = celsius_to_fahrenheit_conversion(celsius)
+    display(fahrenheit, "F")
+
+
+def fahrenheit_to_celsius_input():
+    fahrenheit = float(input("Fahrenheit: "))
+    celsius = fahrenheit_to_celsius_conversion(fahrenheit)
+    display(celsius, "C")
 
 
 def main():
@@ -25,9 +38,9 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            celsius_to_farenheit()
+            celsius_to_fahrenheit_input()
         elif choice == "F":
-            farenheit_to_censius()
+            fahrenheit_to_celsius_input()
         else:
             print("Invalid option")
             print(MENU)

e8d725e525626277e866524d9ded5a69b13a10d7
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-08-23 16:48:51 +1000

    Appended code to the python file to achieve the following; 1. Change the first element of numbers to "ten" (the string, not the number 10) 2. Change the last element of numbers to 1 3. Get all the elements from numbers except the first two (slice) 4. Check if 9 is an element of numbers

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -13,3 +13,19 @@ numbers[3:4] = [1]
 "3" in numbers = False
 numbers + [6, 5, 3] = Appends [6, 5, 3] to the end of the list = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 """
+
+"""Change the first element of numbers to "ten" (the string, not the number 10)"""
+numbers[0] = "ten"
+
+"""Change the last element of numbers to 1"""
+numbers[-1] = 1
+
+"""Get all the elements from numbers except the first two (slice)"""
+print(numbers[2:])
+
+"""Check if 9 is an element of numbers"""
+if (9 in numbers) is True:
+    element_status = "is"
+else:
+    element_status = "isn't"
+print("9 {} an element of numbers".format(element_status))

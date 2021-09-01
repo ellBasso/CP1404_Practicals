be397ea65685b44eff93c1ff63516cd7491cad00
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-09-02 09:57:42 +1000

    Shorten input prompt message to "Text: " Refactor original word counter for loop to use .get

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -3,14 +3,10 @@ Write a program to count the occurrences of words in a string. The program shoul
 then print the counts of how many of each word are in the file.
 """
 
-string_to_count = (input("Please enter a string to count: ")).lower().split(" ")
+string_to_count = (input("Text: ")).lower().split(" ")
 counted_words = {}
 for word in string_to_count:
-    if word in counted_words:
-        counted_words[word] += 1
-    else:
-        counted_words[word] = 1
-
+    counted_words[word] = counted_words.get(word, 0) + 1
 max_word_len = max(len(word) for word in counted_words)
 
 counted_words_sorted = list(counted_words.items())

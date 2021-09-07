931f192c5e9a98a29547dae5d5be80a311cd8469
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-09-07 13:38:48 +1000

    Incorporate guitar class and a method of testing

---
+++ prac_06/guitar.py
@@ -0,0 +1,18 @@
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        """Gibson L-5 CES (1922) : $16,035.40"""
+        return f"{self.name} ({self.year}) : ${self.cost:,.2f}"
+
+    def get_age(self):
+        return 2021 - self.year
+
+    def is_vintage(self):
+        if self.get_age() > 49:
+            return True
+        else:
+            return False

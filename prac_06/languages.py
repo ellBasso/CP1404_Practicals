005a5237d9c6c6fe714afc27f3604842d5372359
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-09-07 13:23:03 +1000

    Create and complete language exercise

---
+++ prac_06/languages.py
@@ -0,0 +1,13 @@
+from programming_language import ProgrammingLanguage
+
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+"""Now create a new list that contains these three existing ProgrammingLanguage objects."""
+languages = [ruby, python, visual_basic]
+"""Loop through and print the names of all the languages with dynamic typing
+(make sure you use your new is_dynamic method!)"""
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)

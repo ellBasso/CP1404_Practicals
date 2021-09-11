a286a4cd5804407050da649a8016b18b15ee6e00
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-09-11 10:46:08 +1000

    Add docstrings

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -1,4 +1,8 @@
+"""Class for languages.py to use"""
+
+
 class ProgrammingLanguage:
+    """The programming Language class to save the information of programming languages"""
     def __init__(self, name, typing, reflection, year):
         self.name = name
         self.typing = typing
@@ -9,4 +13,5 @@ class ProgrammingLanguage:
         return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
 
     def is_dynamic(self):
+        """Returns True or False depending on whether the language's typing is dynamic or not"""
         return self.typing == "Dynamic"

21a708acc815925379f8dd00b48fc9e7ad12d3ef
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-09-13 19:45:21 +1000

    Refactor code into 'show_guitars' function

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -6,15 +6,7 @@ from prac_06.guitar import Guitar
 
 def main():
     guitars = get_guitars()
-    print("These are my guitars:")
-    count = 0
-    for guitar in guitars:
-        if guitar.is_vintage():
-            vintage_string = "(vintage)"
-        else:
-            vintage_string = ""
-        print(f"Guitar {count + 1}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
-        count += 1
+    show_guitars(guitars)
 
 
 def get_guitars():
@@ -29,6 +21,18 @@ def get_guitars():
         print(f"{guitar_to_add} added.")
         name = input("Name: ")
     return guitars
+
+
+def show_guitars(guitars):
+    print("These are my guitars:")
+    count = 0
+    for guitar in guitars:
+        if guitar.is_vintage():
+            vintage_string = "(vintage)"
+        else:
+            vintage_string = ""
+        print(f"Guitar {count + 1}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
+        count += 1
 
 
 main()

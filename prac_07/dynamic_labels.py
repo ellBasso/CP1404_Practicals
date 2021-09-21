402df7192acab01334543f599136ff2ea0d6c8c9
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-09-21 13:38:25 +1000

    Complete dynamic labels exercise

---
+++ prac_07/dynamic_labels.py
@@ -0,0 +1,29 @@
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.label import Label
+from kivy.properties import StringProperty
+
+
+class DynamicLabels(App):
+    status_text = StringProperty()
+
+    def __init__(self, **kwargs):
+        """Construct main app."""
+        super().__init__(**kwargs)
+        self.name_to_phone = {"Bob Ross", "John Johnston", "Jeff Stevens", "Jodie Whittaker"}
+
+    def build(self):
+        """Build the Kivy GUI."""
+        self.title = "Dynamic Labels"
+        self.root = Builder.load_file('dynamic_labels.kv')
+        self.create_widgets()
+        return self.root
+
+    def create_widgets(self):
+        """Create labels from dictionary entries and add them to the GUI."""
+        for name in self.name_to_phone:
+            temp_button = Label(text=name, font_size=48)
+            self.root.ids.main.add_widget(temp_button)
+
+
+DynamicLabels().run()

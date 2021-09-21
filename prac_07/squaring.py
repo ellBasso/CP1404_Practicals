dfb44f28a35a3519f22a10d0ea5bb4664092b008
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-09-21 10:57:55 +1000

    Complete Square Number Exercise Modify code to allow floating numbers Add exception handling

--- prac_07/squaring.py
+++ prac_07/squaring.py
@@ -16,15 +16,18 @@ class SquareNumberApp(App):
     """ SquareNumberApp is a Kivy App for squaring a number """
     def build(self):
         """ build the Kivy app from the kv file """
-        Window.size = (200, 300)
+        Window.size = (400, 200)
         self.title = "Square Number"
         self.root = Builder.load_file('squaring.kv')
         return self.root
 
     def handle_calculate(self, value):
         """ handle calculation (could be button press or other call), output result to label widget """
-        result = value ** 2
-        self.root.ids.output_label.text = str(result)
+        try:
+            result = float(value) ** 2
+            self.root.ids.output_label.text = str(result)
+        except ValueError:
+            pass
 
 
 SquareNumberApp().run()

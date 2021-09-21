ac972b35ea82314cbfeb9da4bd66b1b3455bc7c0
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-09-21 11:35:41 +1000

    Incorporate invalid input handling

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -12,15 +12,21 @@ class MilesConverter(App):
         return self.root
 
     def convert_miles(self):
-        """Set the label to predetermined greeting below"""
-        miles = float(self.root.ids.miles_input.text)
-        self.root.ids.conversion_label.text = str(miles * MILES_CONVERSION)
+        """Converts the miles value entered to kilometers"""
+        self.root.ids.conversion_label.text = str(self.get_valid_miles() * MILES_CONVERSION)
 
     def handle_increment(self, incr):
         increment = int(incr)
-        miles = float(self.root.ids.miles_input.text)
-        self.root.ids.miles_input.text = str(miles + increment)
+        self.root.ids.miles_input.text = str(self.get_valid_miles() + increment)
         self.convert_miles()
+
+    def get_valid_miles(self):
+        """Validates the miles value inputted"""
+        try:
+            miles = float(self.root.ids.miles_input.text)
+            return miles
+        except ValueError:
+            return 0
 
 
 MilesConverter().run()

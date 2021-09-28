86b934f54aeafb3402987a9c549926314ccce11f
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-09-28 15:19:50 +1000

    Add the class variable price_per_km to the Taxi class Change the __init__ function so that it does not take in the price_per_km Change taxi_test.py to not pass price_per_km

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -2,7 +2,7 @@
 
 from prac_08.taxi import Taxi
 
-Prius_1 = Taxi("Prius 1", 100, 1.23)  # Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
+Prius_1 = Taxi("Prius 1", 100)  # Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
 Prius_1.drive(40)  # Drive the taxi 40 km
 print(Prius_1)  # Print the taxi's details and the current fare
 Prius_1.start_fare()  # Restart the meter (start a new fare)

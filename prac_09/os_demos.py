bcf13447b75781d659a878a4b87d06113800c028
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-10-19 12:37:01 +1000

    Incorporate loop to rename all music files

--- prac_09/os_demos.py
+++ prac_09/os_demos.py
@@ -48,14 +48,29 @@ def get_fixed_filename(filename):
 def demo_walk():
     """Process all subdirectories using os.walk()."""
     os.chdir('Lyrics')
+    directories = []
     for directory_name, subdirectories, filenames in os.walk('.'):
         print("Directory:", directory_name)
         print("\tcontains subdirectories:", subdirectories)
         print("\tand files:", filenames)
         print("(Current working directory is: {})".format(os.getcwd()))
+        directories.append(directory_name)
+    print(directories)
+    origin_directory = os.getcwd()
+    for directory in directories:
+        os.chdir(os.path.join(origin_directory, directory))
+        for filename in os.listdir('.'):
+            # Ignore directories, just process files
+            print(filename)
+            if os.path.isdir(filename):
+                continue
 
-        # TODO: add a loop to rename the files
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(filename, new_name))
+
+            # Rename file to new name - in place
+            os.rename(filename, new_name)
 
 
-main()
-# demo_walk()
+# main()
+demo_walk()

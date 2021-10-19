79c26492d8b2f993a40f797fbd8c98e501a26878
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-10-20 09:03:07 +1000

    Complete intermediate exercise. Incorporate algorithm to add underlines to PascalCase named files. Add method to distinguish whether filename has already been modified from previous algorithm

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -32,14 +32,30 @@ def main():
         new_name = get_fixed_filename(filename)
         print("Renaming {} to {}".format(filename, new_name))
 
-        # Option 1: rename file to new name - in place
-        # os.rename(filename, new_name)
+        #Rename file to new name - in place
+        os.rename(filename, new_name)
 
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
     new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
-    return new_name
+    modified = False
+    for character in new_name:
+        if character == '_':
+            modified = True
+            return new_name
+    if not modified:
+        file_name = []
+        for number, character in enumerate(new_name):
+            if (character.isupper()) and (number > 0):
+                if (new_name[number - 1]).islower():
+                    file_name.append(("_{}".format(character)))
+                elif (new_name[number - 1]).isupper() and (character.isupper()):
+                    file_name.append("_{}".format(character))
+            else:
+                file_name.append(character)
+        file_name = ''.join(file_name)
+        return file_name
 
 
 main()

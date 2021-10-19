4eac2a6c063568e53c8b4e83d9f497b2585847f5
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-10-20 09:50:05 +1000

    Refactor code to assign file extension to folder; Do via user input

--- prac_09/sort_files_2.py
+++ prac_09/sort_files_2.py
@@ -11,21 +11,33 @@ def main():
     # Print a list of all files in current directory
     print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
 
-    # Create folders for filename extensions
+    extensions = {}
+
+    # Determine folder category for filename extensions
     for file in os.listdir('.'):
         try:
-            os.mkdir((file.split('.'))[1])
-            print("Made folder {}".format((file.split('.'))[1]))
-        except FileExistsError:
+            extension = (file.split('.'))[1]
+            extensions[extension]
             pass
-        except IndexError:
-            pass
+        except KeyError:
+            valid_answer = False
+            while not valid_answer:
+                category = input("What category are {} file extensions? ".format(extension))
+                if category.isalpha():
+                    valid_answer = True
+                    extensions[extension] = category
+                    try:
+                        os.mkdir(category)
+                    except FileExistsError:
+                        pass
+                else:
+                    print("Please give a text answer.")
 
     # Move each file into suitable directory based on its file extension
     for file in os.listdir('.'):
         if os.path.isfile(file):
             file_extension = (file.split('.'))[1]
-            file_destination = os.path.join(file_extension, file)
+            file_destination = os.path.join(extensions[file_extension], file)
             print(file_destination)
             shutil.move(file, file_destination)
 

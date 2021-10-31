ff658af8d0403e871f814b766d0856b244af76ee
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-10-31 20:41:02 +1000

    Create Fahrenheit and Celsius converter

--- prac_10/app.py
+++ prac_10/app.py
@@ -13,5 +13,15 @@ def greet(name=""):
     return 'Hello {}'.format(name)
 
 
+@app.route('/f/<celsius>')
+def celsius_to_fahrenheit(celsius):
+    try:
+        result = (float(celsius) * float(9 / 5)) + float(32)
+        return f"""<h1>Fahrenheit: {result}</h1>
+        <h1>Celsius: {celsius}</h1>"""
+    except:
+        return "Please enter a valid number"
+
+
 if __name__ == '__main__':
     app.run()

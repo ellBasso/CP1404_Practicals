a101f9ad7c287ed6ed4650f865710c2a0d99c0d7
ellBasso <87956013+ellBasso@users.noreply.github.com>
2021-10-31 20:28:00 +1000

    Setup greet route and simple page

--- prac_10/app.py
+++ prac_10/app.py
@@ -8,5 +8,10 @@ def hello_world():  # put application's code here
     return '<H1>Hello World!</h1>'
 
 
+@app.route('/greet')
+def greet():
+    return 'Hello'
+
+
 if __name__ == '__main__':
     app.run()
